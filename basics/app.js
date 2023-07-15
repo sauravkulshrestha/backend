29910859c819ef67c4a1da78c67d41ccc5a6c466
Saurav Kulshrestha <saurav.external@credable.in>
2023-07-15 16:13:29 +0530

    Express.js , implemeted riderction and 404 page

---
+++ basics/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const app = express();
+
+app.listen(3000);
+
+app.get('/', function (req, res) { 
+    // res.send("<h1>hello express world!</h1>");
+    res.sendFile("templets/file.html", { root:__dirname});
+})
+
+app.get('/about', function (req, res) { 
+    // res.send("<h1>hello express world!</h1>");
+    res.sendFile("templets/about.html", {root:__dirname});
+})
+
+// redirects
+
+app.get("/about-us" , (req , res) => {
+    res.redirect('/about');
+});
+
+// 404 page 
+
+app.use((req, res) => {
+    // res.status(404); 
+    res.status(404).sendFile('templets/404.html', { root: __dirname }); // status code send by chaing method
+})
